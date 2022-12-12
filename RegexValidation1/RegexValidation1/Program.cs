240c5cd572a81207506c533013ad5842105a0e8c
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-12-12 17:23:36 +0530

    Check last alphbet or special charcter should not

--- RegexValidation1/RegexValidation1/Program.cs
+++ RegexValidation1/RegexValidation1/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace RegexValidation1
+﻿using System.Text.RegularExpressions;
+
+namespace RegexValidation1
 {
     internal class Program
     {
@@ -14,6 +16,12 @@
 
             bool alphabet_SpecialChar = pattern.validatePincode("A400088");
             Console.WriteLine("Alphabet : " + alphabet_SpecialChar);
+
+            string pattern1 = @"^[1-9]{6}[A-Z]{7}$";
+
+            Regex rgobj = new Regex(pattern1);
+            bool result1 = rgobj.IsMatch("400088B");
+            Console.WriteLine("Check not last alphabet " + result1);
 
         }
     }
